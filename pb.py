e1eb6be504bdc7c410697ef5a130ade886c83371
avargas94 <andrewvargas6933@gmail.com>
2018-02-21 00:50:14 +0000

    Completed Assignment

--- pb.py
+++ pb.py
@@ -34,7 +34,7 @@ def constructDrink(x):
             ing = ingredients[f]
             r.update({f: random.choice(ing)})
     
-    print("With the what your taste buds are feeling right now. Your drink will have the following ingredients!")
+    print("With the what your taste buds are feeling right now. This is what I have selected for you!")
     for d, n in r.items(): print(n)
     return n
 
